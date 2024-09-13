539dbfadd28a283648b8541aa03ad7f4cf9b809b
stoneys1 <malvesbonacina@yahoo.com.br>
2024-09-13 11:20:55 -0300

    commit 1

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from '../../assets/logo-dio.png';
+
+import { Button } from '../Button';
+
+import { Container, Wrapper, BuscarInputContainer, Input, Row, Menu, MenuRight, UserPicture} from './styles';
+
+const Header = ({autenticado}) => {
+  return (
+    <Wrapper>
+      <Container>
+          <Row>
+            <img src={logo} alt="Logo da dio"/>
+            {autenticado ? (
+              <>
+               <BuscarInputContainer>
+                <Input placeholder='Buscar...'/>
+               </BuscarInputContainer>
+                <Menu>Live Code</Menu>
+                <Menu>Global</Menu>
+              </>
+            ) : null}
+          </Row>
+          <Row>
+              {autenticado ? (
+                <UserPicture src="https://avatars.githubusercontent.com/u/98463307?s=400&u=7573121ebdf0cc9d1e63cf124394033a4f05cc39&v=4"/>
+              ) : (
+              <>
+                <MenuRight href="/">Home</MenuRight>
+                <Button title="Entrar" />
+                <Button title="Cadastrar" />
+              </>)}
+          </Row>
+      </Container>
+    </Wrapper>
+  )
+}
+
+export { Header }
